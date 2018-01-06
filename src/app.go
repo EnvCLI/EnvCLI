a1fd1eb1dcd141cf42704fe27e38847d09bd40fe
Philipp Heuer <git@philippheuer.me>
2018-01-06 02:45:22 +0100

    feature(core): initial commit

---
+++ src/app.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"time"
+	"fmt"
+	"strings"
+	"sort"
+	log "github.com/sirupsen/logrus" // imports as package "log"
+	"gopkg.in/urfave/cli.v2" // imports as package "cli"
+)
+
+// Init Hook
+func init() {
+	log.SetOutput(os.Stdout)
+  log.SetLevel(log.DebugLevel)
+}
+
+// CLI Main Entrypoint
+func main() {
+	app := &cli.App{
+    Name: "EnvCLI Utility",
+		Version: "v0.1.1",
+    Compiled: time.Now(),
+		EnableShellCompletion: true,
+		Authors: []*cli.Author{
+      &cli.Author{
+        Name:  "Philipp Heuer",
+        Email: "git@philippheuer.me",
+      },
+    },
+    Usage: "Runs cli commands within docker containers to provide a modern development environment",
+		Flags: []cli.Flag{
+      &cli.StringFlag{
+        Name:  "loglevel",
+        Value: "info",
+        Usage: "The loglevel used by envcli, use this to troubleshoot issues",
+      },
+    },
+		Commands: []*cli.Command{
+      {
+        Name:    "self-update",
+        Aliases: []string{},
+        Usage:   "updates the dev cli utility",
+        Action:  func(c *cli.Context) error {
+					// Set loglevel
+					setLoglevel(c.String("loglevel"))
+
+					// Placeholder
+          return nil
+        },
+      },
+			{
+        Name:    "run",
+        Aliases: []string{},
+        Usage:   "runs 3rd party commands within their respective docker containers",
+        Action:  func(c *cli.Context) error {
+					// Set loglevel
+					setLoglevel(c.String("loglevel"))
+
+					// parse command
+					commandName := c.Args().First()
+					commandWithArguments := strings.Join(append([]string{commandName}, c.Args().Tail()...), " ")
+					log.Debugf("Command run in Remote: %s | %s", commandName, commandWithArguments)
+
+					// load yml project configuration
+					configurationLoader := ConfigurationLoader{}
+					var config ProjectConfigrationFile = configurationLoader.load(configurationLoader.getWorkingDirectory() + "/.envcli.yml")
+
+					// check for command prefix and get the matching configuration entry
+					var dockerImage string
+					var dockerImageTag string
+					var projectDirectory string
+					for _, element := range config.Commands {
+						if element.Name == commandName {
+							log.Debugf("Found matching entry in configuration for command %s [%s]", commandName, element.Description)
+							dockerImage = element.Image
+							dockerImageTag = element.Tag
+							projectDirectory = element.ProjectDirectory
+							log.Debugf("Image: %s | Tag: %s", dockerImage, dockerImageTag)
+						}
+					}
+
+					// detect container service and send command
+					// - docker native
+					log.Infof("Redirecting command to Docker Container [%s:%s].", dockerImage, dockerImageTag)
+					var command string = fmt.Sprintf("docker run -it --rm --volume \"${PWD}:%s\" -w /project %s:%s %s", projectDirectory, dockerImage, dockerImageTag, commandWithArguments)
+					log.Debugf("RAW Command: %s", command)
+					cmd := exec.Command("apt", "install", "-y", "fail2ban")
+					cmd.Stdout = os.Stdout
+					if err := cmd.Start(); err != nil {
+			        log.Fatal(err)
+			    }
+			    cmd.Wait()
+
+          return nil
+        },
+      },
+    },
+  }
+
+	// Sort Flags & Commands by Alphabet
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	// Run Application
+  app.Run(os.Args)
+}
+
+/**
+ * Sets the loglevel according to the flag on each command run
+ */
+func setLoglevel(loglevel string) {
+	if loglevel == "info" {
+		log.SetLevel(log.InfoLevel)
+	} else if loglevel == "debug" {
+		log.SetLevel(log.DebugLevel)
+	}
+}
---
+++ src/config.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+	log "github.com/sirupsen/logrus" // imports as package "log"
+	"github.com/jinzhu/configor" // imports as package "configor"
+)
+
+/**
+ * The Project Configuration and it's properties
+ */
+type ConfigurationLoader struct {
+}
+
+/**
+ * The Project Configuration
+ */
+type ProjectConfigrationFile struct {
+	Commands []struct {
+		Name  string
+		Description string
+		Image string
+		Tag string
+		ProjectDirectory string `default:"/project"`
+	}
+}
+
+/**
+ * Load the .devcli.yml Configuration
+ */
+func (configurationLoader ConfigurationLoader) load(configFile string) ProjectConfigrationFile {
+	var cfg ProjectConfigrationFile;
+
+	log.Debug("Loading project configuration file " + configFile)
+	configor.New(&configor.Config{Debug: false}).Load(&cfg, configFile)
+
+ 	return cfg
+}
+
+/**
+ * Get the working directory
+ */
+func (configurationLoader ConfigurationLoader) getWorkingDirectory() string {
+	workingDir, err := os.Getwd()
+	if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Fatal("Couldn't detect working directory!")
+	}
+
+ 	return workingDir
+}
