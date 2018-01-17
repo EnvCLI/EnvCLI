a95620ab9b5cb7b17c9f84f20ab54b3a55b4ffc3
Philipp Heuer <git@philippheuer.me>
2018-01-17 23:21:24 +0100

    fix(run): fix system exec command execution for linux based systems

--- src/util.go
+++ src/util.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-  "fmt"
 	"os"
 	"os/exec"
-	"strings"
 	"runtime"
 	log "github.com/sirupsen/logrus" // imports as package "log"
 )
@@ -13,25 +11,29 @@ import (
  * CLI Command Passthru with input/output
  */
 func systemExec(command string) {
-	// Use Powershell on Windows
-	if runtime.GOOS == "windows" {
-		command = fmt.Sprintf("powershell %s", command)
-	}
-
 	log.Debugf("Running Command: %s", command)
 
-	// Arguments
-	args := strings.Fields(command)
-
 	// Run Command
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("Failed to execute command: %s\n", err.Error())
-		os.Exit(1)
+  if runtime.GOOS == "linux" {
+    cmd := exec.Command("/usr/bin/env", "sh", "-c", command)
+    cmd.Stdin = os.Stdin
+  	cmd.Stdout = os.Stdout
+  	cmd.Stderr = os.Stderr
+  	err := cmd.Run()
+  	if err != nil {
+  		log.Fatalf("Failed to execute command: %s\n", err.Error())
+  		os.Exit(1)
+  	}
+  } else if runtime.GOOS == "windows" {
+    cmd := exec.Command("powershell", command)
+    cmd.Stdin = os.Stdin
+  	cmd.Stdout = os.Stdout
+  	cmd.Stderr = os.Stderr
+  	err := cmd.Run()
+  	if err != nil {
+  		log.Fatalf("Failed to execute command: %s\n", err.Error())
+  		os.Exit(1)
+  	}
 	}
 }
 
