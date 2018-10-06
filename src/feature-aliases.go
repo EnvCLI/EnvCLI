7f099e4e7a14934bdbedc3da2a09811a3133a8ca
Philipp Heuer <git@philippheuer.me>
2018-10-06 14:29:11 +0200

    feature: add support for aliases on linux #1

---
+++ src/feature-aliases.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus" // imports as package "log"
+	"os"
+	"runtime"
+)
+
+/**
+ * CLI Command Passthru with input/output
+ */
+func installAlias(command string, scope string) error {
+	log.Debugf("Creating alias for command: %s [Scope: %s]", command, scope)
+
+	// download alias script for each used command
+	if runtime.GOOS == "linux" {
+		log.Debugf("Detected Linux - Will place bash scripts into PATH ...")
+		aliasScriptURL := "https://raw.githubusercontent.com/EnvCLI/EnvCLI/develop/scripts/alias.sh"
+		aliasScriptFilepath := configurationLoader.getExecutionDirectory() + "/" + command + ".sh"
+
+		err := DownloadFile(aliasScriptFilepath, aliasScriptURL)
+		if err != nil {
+			log.Errorf("Can't create alias [%s], download failed.", command)
+			panic(err)
+		} else {
+			log.Debugf("Created alias for [%s]!", command)
+
+			// set execute permissions
+			chmodErr := os.Chmod(aliasScriptFilepath, 0744)
+			if chmodErr != nil {
+				log.Errorf("Failed to make the alias script for [%s] executable!", command)
+			} else {
+				log.Debugf("Made alias script for [%s] executable!", command)
+			}
+		}
+	} else {
+		log.Errorf("Can't create alias for [%s]. Aliases aren't supported on %s yet!", command, runtime.GOOS)
+	}
+
+	return nil
+}
--- src/util.go
+++ src/util.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus" // imports as package "log"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
 	"reflect"
@@ -130,4 +132,32 @@ func detectCIEnvironment() (val bool) {
 	}
 
 	return false
+}
+
+/**
+ * DownloadFile will download a url to a local file.
+ */
+func DownloadFile(filepath string, url string) error {
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
