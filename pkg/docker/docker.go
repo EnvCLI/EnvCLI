c84365d2044c7ac2756429ee662ddb33df6c6ffc
Taylan Dogan <doganntaylan@gmail.com>
2018-12-28 23:37:35 +0300

    Refactor Volume Mounting in Docker

--- pkg/docker/docker.go
+++ pkg/docker/docker.go
@@ -11,6 +11,29 @@ import (
 	isatty "github.com/mattn/go-isatty"
 )
 
+// Mounting volumes
+func volumeMount(shellCommand *bytes.Buffer, mounts *[]ContainerMount) {
+	for _, containerMount := range *mounts {
+		if containerMount.MountType == "directory" {
+			var mountSource = containerMount.Source
+			var mountTarget = containerMount.Target
+			// docker toolbox doesn't support direct mounts, so we have to use the shared folder feature
+			if isDockerToolbox() && runtime.GOOS == "windows" {
+				driveLetters := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+				for _, element := range driveLetters {
+					mountSource = strings.Replace(mountSource, element+":\\", "/"+element+"_DRIVE/", 1)
+				}
+
+				// replace windows path separator with linux path separator
+				mountSource = strings.Replace(mountSource, "\\", "/", -1)
+			}
+
+			shellCommand.WriteString(fmt.Sprintf("--volume \"%s:%s\" ", mountSource, mountTarget))
+		}
+	}
+
+}
+
 // TODO: This function does more than one job.
 //       Split this into another functions
 func ContainerExec(image string, commandShell string, command string, mounts []ContainerMount, workingdir string, environment []string, publish []string) {
@@ -47,25 +70,7 @@ func ContainerExec(image string, commandShell string, command string, mounts []C
 	// - set working directory
 	shellCommand.WriteString(fmt.Sprintf("--workdir %s ", workingdir))
 	// - volume mounts
-	for _, containerMount := range mounts {
-		if containerMount.MountType == "directory" {
-			var mountSource = containerMount.Source
-			var mountTarget = containerMount.Target
-			// docker toolbox doesn't support direct mounts, so we have to use the shared folder feature
-			if isDockerToolbox() && runtime.GOOS == "windows" {
-				driveLetters := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-				for _, element := range driveLetters {
-					mountSource = strings.Replace(mountSource, element+":\\", "/"+element+"_DRIVE/", 1)
-				}
-
-				// replace windows path separator with linux path separator
-				mountSource = strings.Replace(mountSource, "\\", "/", -1)
-			}
-
-			shellCommand.WriteString(fmt.Sprintf("--volume \"%s:%s\" ", mountSource, mountTarget))
-		}
-	}
-
+	volumeMount(&shellCommand, &mounts)
 	// - image
 	shellCommand.WriteString(fmt.Sprintf("%s ", image))
 	// - command to run inside of the container
