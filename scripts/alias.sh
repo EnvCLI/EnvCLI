42dec4bfc5fd49f53611a53ce55dc6bdf31f4c76
Philipp Heuer <git@philippheuer.me>
2019-08-31 18:38:54 +0200

    fix: quote all args before passing them to envcli

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -3,5 +3,11 @@
 # find out which alias is called
 aliasFor=$(echo $(basename $(readlink -nf $0)) | cut -f 1 -d '.')
 
+# quote all args
+for arg in "$@"; do 
+    arg="${arg//\\/\\\\}"
+    allargs="$allargs \"${arg//\"/\\\"}\""
+done
+
 # call envcli for the alias and pass all arguments
-envcli run $aliasFor $@
+envcli run $aliasFor $allargs
